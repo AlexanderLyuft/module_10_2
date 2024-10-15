483d49756611cf45c07a8c0c83d88e8c01bcd3f3
AlexanderLyuft <alexanderlyuft@mail.ru>
2024-10-15 20:41:00 +0700

    Add files via upload

---
+++ module_10_2.py
@@ -0,0 +1,39 @@
+            # Домашнее задание по теме "Потоки на классах"
+
+
+from time import sleep
+from threading import Thread
+
+
+class Knight(Thread):
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        enemies = 100
+        days = int(enemies / self.power)
+        for i in range(days):
+            enemies -= self.power
+            print(f'{self.name} сражается {i + 1} день(дня), осталось {enemies} воинов.', end='\n')
+            sleep(1)
+        print(f'{self.name} одержал победу спустя {days} дней!')
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight('Sir Galahad', 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+print('Все битвы закончились!')
+
+
+
+
+
+
